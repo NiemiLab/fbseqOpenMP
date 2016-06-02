c1c976381eeed5401a5087eaa30bbd6f3726d482
wlandau <will.landau@gmail.com>
2016-06-02 06:53:15 -0400

    Guard against underflow in slice_horseshoe.h

--- src/sampler/slice_horseshoe.h
+++ src/sampler/slice_horseshoe.h
@@ -12,7 +12,8 @@ double exp_cdf(double x, double rate){
 }
 
 double exp_invcdf(double u, double rate){
-  return -log(1.0 - u)/rate;
+  double tol = 1e-12;
+  return -log(1.0 - u - tol)/(rate + tol);
 }
 
 args_t slice_horseshoe(chain_t *dd, args_t args){
