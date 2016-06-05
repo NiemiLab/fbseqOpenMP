55a38ba24cc9820fce88b749b197b715f54d4ad2
wlandau <will.landau@gmail.com>
2016-06-04 20:46:42 -0400

    Divide first in estimates_update.h
    Function wh is now pcs (precise cumulative sum). Divide
    first for extra precision in floating point arithmetic.

--- src/util/estimates_update.h
+++ src/util/estimates_update.h
@@ -2,37 +2,39 @@
 #define UTIL_ESTIMATES_UPDATE_H
 
 /*
- * Numerically stable cumulative sum due to Welford, B. P. (1962),
+ * Precise cumulative sum due to Welford, B. P. (1962),
  * "Note on a method for calculating corrected sums of squares and products",
  * Technometrics 4(3), 419–420.
  */
-double wf(double x_n, double x_mean, int n){
-  double diff = x_n - x_mean;
-  diff /= ((double) n);
+double pcs(double x_n, double x_mean, int n){
+  double dn = (double) n;
+  double y1 = x_n/dn;
+  double y2 = x_mean/dn;
+  double diff = y1 - y2;
   return x_mean + diff;
 }
 
 void estimates_update_kernel1(chain_t *dd, int m){
   int l;
 
-  dd->nuPostMean[0] = wf(dd->nu[0], dd->nuPostMean[0], m);
-  dd->tauPostMean[0] = wf(dd->tau[0], dd->tauPostMean[0], m);
+  dd->nuPostMean[0] = pcs(dd->nu[0], dd->nuPostMean[0], m);
+  dd->tauPostMean[0] = pcs(dd->tau[0], dd->tauPostMean[0], m);
 
   for(l = 0; l < dd->L; ++l){
-    dd->sigmaSquaredPostMean[l] = wf(dd->sigmaSquared[l], dd->sigmaSquaredPostMean[l], m);
-    dd->thetaPostMean[l] = wf(dd->theta[l], dd->thetaPostMean[l], m);
+    dd->sigmaSquaredPostMean[l] = pcs(dd->sigmaSquared[l], dd->sigmaSquaredPostMean[l], m);
+    dd->thetaPostMean[l] = pcs(dd->theta[l], dd->thetaPostMean[l], m);
   }
 }
 
 void estimates_update_kernel2(chain_t *dd, int m){
   int l;
 
-  dd->nuPostMeanSquare[0] = wf(dd->nu[0]*dd->nu[0], dd->nuPostMeanSquare[0], m);
-  dd->tauPostMeanSquare[0] = wf(dd->tau[0]*dd->tau[0], dd->tauPostMeanSquare[0], m);
+  dd->nuPostMeanSquare[0] = pcs(dd->nu[0]*dd->nu[0], dd->nuPostMeanSquare[0], m);
+  dd->tauPostMeanSquare[0] = pcs(dd->tau[0]*dd->tau[0], dd->tauPostMeanSquare[0], m);
 
   for(l = 0; l < dd->L; ++l){
-    dd->sigmaSquaredPostMeanSquare[l] = wf(dd->sigmaSquared[l]*dd->sigmaSquared[l], dd->sigmaSquaredPostMeanSquare[l], m);
-    dd->thetaPostMeanSquare[l] = wf(dd->theta[l]*dd->theta[l], dd->thetaPostMeanSquare[l], m);
+    dd->sigmaSquaredPostMeanSquare[l] = pcs(dd->sigmaSquared[l]*dd->sigmaSquared[l], dd->sigmaSquaredPostMeanSquare[l], m);
+    dd->thetaPostMeanSquare[l] = pcs(dd->theta[l]*dd->theta[l], dd->thetaPostMeanSquare[l], m);
   }
 }
 
@@ -40,13 +42,13 @@ void estimates_update_kernel3(chain_t *dd, int m){
   int id, l, n;
   #pragma omp parallel for num_threads(dd->threads) private(l, n)
   for(id = IDX; id < dd->G; id += NTHREADSX){
-    dd->gammaPostMean[id] = wf(dd->gamma[id], dd->gammaPostMean[id], m);
+    dd->gammaPostMean[id] = pcs(dd->gamma[id], dd->gammaPostMean[id], m);
     for(l = 0; l < dd->L; ++l){
-      dd->betaPostMean[I(l, id)] = wf(dd->beta[I(l, id)], dd->betaPostMean[I(l, id)], m);
-      dd->xiPostMean[I(l, id)] = wf(dd->xi[I(l, id)], dd->xiPostMean[I(l, id)], m);
+      dd->betaPostMean[I(l, id)] = pcs(dd->beta[I(l, id)], dd->betaPostMean[I(l, id)], m);
+      dd->xiPostMean[I(l, id)] = pcs(dd->xi[I(l, id)], dd->xiPostMean[I(l, id)], m);
     }
     for(n = 0; n < dd->N; ++n)
-      dd->epsilonPostMean[I(n, id)] = wf(dd->epsilon[I(n, id)], dd->epsilonPostMean[I(n, id)], m);
+      dd->epsilonPostMean[I(n, id)] = pcs(dd->epsilon[I(n, id)], dd->epsilonPostMean[I(n, id)], m);
   }
 }
 
@@ -54,13 +56,13 @@ void estimates_update_kernel4(chain_t *dd, int m){
   int id, l, n;
   #pragma omp parallel for num_threads(dd->threads) private(l, n)
   for(id = IDX; id < dd->G; id += NTHREADSX){
-    dd->gammaPostMeanSquare[id] = wf(dd->gamma[id]*dd->gamma[id], dd->gammaPostMeanSquare[id], m);
+    dd->gammaPostMeanSquare[id] = pcs(dd->gamma[id]*dd->gamma[id], dd->gammaPostMeanSquare[id], m);
     for(l = 0; l < dd->L; ++l){
-      dd->betaPostMeanSquare[I(l, id)] = wf(dd->beta[I(l, id)]*dd->beta[I(l, id)], dd->betaPostMeanSquare[I(l, id)], m);
-      dd->xiPostMeanSquare[I(l, id)] = wf(dd->xi[I(l, id)]*dd->xi[I(l, id)], dd->xiPostMeanSquare[I(l, id)], m);
+      dd->betaPostMeanSquare[I(l, id)] = pcs(dd->beta[I(l, id)]*dd->beta[I(l, id)], dd->betaPostMeanSquare[I(l, id)], m);
+      dd->xiPostMeanSquare[I(l, id)] = pcs(dd->xi[I(l, id)]*dd->xi[I(l, id)], dd->xiPostMeanSquare[I(l, id)], m);
     }
     for(n = 0; n < dd->N; ++n)
-      dd->epsilonPostMeanSquare[I(n, id)] = wf(dd->epsilon[I(n, id)]*dd->epsilon[I(n, id)], dd->epsilonPostMeanSquare[I(n, id)], m);
+      dd->epsilonPostMeanSquare[I(n, id)] = pcs(dd->epsilon[I(n, id)]*dd->epsilon[I(n, id)], dd->epsilonPostMeanSquare[I(n, id)], m);
   }
 }
 
@@ -76,11 +78,11 @@ void estimates_update_kernel5(chain_t *dd, int m){
         if(!dd->propositions[Ipropositions(p, c)]) continue;
         contrast = 0.0;
         for(l = 0; l < dd->L; ++l)
-          contrast = wf(dd->contrasts[Icontrasts(c, l)] * dd->beta[I(l, g)], contrast, l + 1);
+          contrast = pcs(dd->contrasts[Icontrasts(c, l)] * dd->beta[I(l, g)], contrast, l + 1);
         truth *= (contrast > dd->bounds[c]);
       }
 
-      dd->probs[I(p, g)] = wf(truth, dd->probs[I(p, g)], m);
+      dd->probs[I(p, g)] = pcs(truth, dd->probs[I(p, g)], m);
     }
   }
 }
